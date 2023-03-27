a46a4d0d84e9542011804f1bb7f6089d4675d53e
Felipe Fantato <67489901+FelipeFantato@users.noreply.github.com>
2023-03-27 14:59:10 -0300

    Seguimento do desafio

--- src/main/java/br/com/dio/desafio/dominio/Mentoria.java
+++ src/main/java/br/com/dio/desafio/dominio/Mentoria.java
@@ -1,4 +1,34 @@
 package br.com.dio.desafio.dominio;
 
-public class Mentoria {
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo{
+
+    public Mentoria() {
+    }
+    private LocalDate data;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
 }
