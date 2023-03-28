d6056c986c6903df95958ea13c4a336669df6bbf
Felipe Fantato <67489901+FelipeFantato@users.noreply.github.com>
2023-03-28 10:08:21 -0300

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -21,7 +23,36 @@ public class Main {
         mentoria.setCargaHoraria(10);
         mentoria.setDescricao("aulas de mentoria py");
 
-        System.out.println(curso1.getCargaHoraria());
-        System.out.println(curso2.getCargaHoraria());
+//        System.out.println(curso1.getCargaHoraria());
+//        System.out.println(curso2.getCargaHoraria());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java developer");
+        bootcamp.setDescricao("Descrição do bootcamp");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Felipe");
+        dev1.inscreverBootCamp(bootcamp);
+
+        System.out.println("Conteudos inscritos "+ dev1.getConteudoInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Conteudos Concluidos "+ dev1.getConteudosConcluidos());
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Joao");
+        dev2.inscreverBootCamp(bootcamp);
+        System.out.println("Conteudos inscritos "+ dev2.getConteudoInscritos());
+        dev2.progredir();
+        dev2.progredir();
+        dev2.progredir();
+
+        System.out.println("Conteudos progrediu? "+ dev2.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos "+ dev2.getConteudosConcluidos());
+        System.out.println("Xp: " + dev2.calcularTotalXp());
     }
 }
